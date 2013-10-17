8addc77b2dabfdbc8e6d8e334cb941af47c0c80f
Peter Fern <github@obfusc8.org>
2013-10-17 18:11:05 +1100

    Generated plugin

---
+++ lib/has_cache/version.rb
@@ -0,0 +1,3 @@
+module HasCache
+  VERSION = "0.0.1"
+end
---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = 'e1e245156149394d3b9db5c857e9bf3619744c38ba2a0fd2f48379e30f52383d897ca2862b5a43e946abade6699c248a4eafba8666262a3a000b466aec12fbd9'
